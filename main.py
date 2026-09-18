3059ecd6523cd0d341e72c4ca3d808f6251201a6
MM21x <mateuszmichgo@gmail.com>
2026-09-18 21:38:04 +0100

    added speed trace comparison plot

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os #creates cache folder 
 import fastf1 #library for f1 data
+import fastf1.plotting #plot styling + team colours
+from matplotlib import pyplot as plt
 
 #Create and enable a local cache so session data is reused between runs
 os.makedirs("cache", exist_ok=True)
 fastf1.Cache.enable_cache("cache")
+
+#set up FastF1 plotting style
+fastf1.plotting.setup_mpl
 
 #Build session data for 2024 monaco quali
 session = fastf1.get_session(2024, "Monaco", "Q")
@@ -21,6 +26,10 @@ ver_lap = session.laps.pick_drivers("VER").pick_fastest()
 #get telemetary for each lap
 lec_tel = lec_lap.get_car_data().add_distance()
 ver_tel = ver_lap.get_car_data().add_distance()
+
+# get team colours for the plot lines
+lec_color = fastf1.plotting.get_team_color(lec_lap["Team"], session=session)
+ver_color = fastf1.plotting.get_team_color(ver_lap["Team"], session=session)
 
 #print key info to test setup
 print("Session loaded successfully")
@@ -53,3 +62,16 @@ print(
     .to_string(index=False)
 )
 print()
+
+#plotting both speed traces on the same graph
+fig, ax = plt.subplots(figsize=(12,6))
+ax.plot(lec_tel["Distance"], lec_tel["Speed"], color=lec_color, label="LEC")
+ax.plot(ver_tel["Distance"], ver_tel["Speed"], color=ver_color, label="VER")
+
+ax.set_title(f"{session.event['EventName']} {session.event.year} Qualifying\nFastest Lap Speed Comparison")
+ax.set_xlabel("Distance (m)")
+ax.set_ylabel("Speed (km/h)")
+ax.legend()
+
+plt.tight_layout()
+plt.show()
